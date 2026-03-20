91ab62c3c8ff0cf0219c6981f43ff1dcc08dff88
TanishqJangir <tanishqjangir001@gmail.com>
2026-03-21 00:33:18 +0530

    corrected the alignment of card in share vault

--- src/pages/SharedVault.tsx
+++ src/pages/SharedVault.tsx
@@ -153,7 +153,7 @@ export const SharedVault = () => {
                         </p>
                     </div>
                 ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 items-start">
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 px-6 py-4">
                         {data.contents.map((item) => (
                             <Card
                                 key={item._id}
